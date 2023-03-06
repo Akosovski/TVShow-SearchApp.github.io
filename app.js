4aef189b22c96eedca9edd085463b34d820f3731
Muhammad Akhta <akhtacaesar@gmail.com>
2023-03-06 20:20:46 +0700

    Functioning Search Bar

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const form = document.querySelector("#searchForm");
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
     // console.dir(form); // Show Directory for APIs
-    const searchTerm = form.elements.query.value
+    const searchTerm = form.elements.query.value;
+    console.log(searchTerm);
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-    console.log(res.data[0].show.image.medium);
+  
+    const img = document.createElement('IMG');
+    img.src = res.data[0].show.image.medium;
+    document.body.append(img);
 })
