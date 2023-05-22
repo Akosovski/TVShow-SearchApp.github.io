f2ac768b97020164a2a3001b8a797836e2fa1755
Muhammad Akhta <akhtacaesar@gmail.com>
2023-05-22 07:46:47 +0700

    Fixed Image Show

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const form = document.querySelector("#searchForm");
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    // console.dir(form); // Show Directory for APIs
     const searchTerm = form.elements.query.value;
-    console.log(searchTerm);
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-  
-    const img = document.createElement('IMG');
-    img.src = res.data[0].show.image.medium;
-    document.body.append(img);
+    makeImages(res.data)
 })
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        console.log(result);
+        if (result.show.image) {
+            const img = document.createElement('IMG');
+            img.src = result.show.image.medium;
+            document.body.append(img);
+        }
+    }
+}
