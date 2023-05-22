b932f5ac70a627522974a31eab7c2488339ef4fa
Muhammad Akhta <akhtacaesar@gmail.com>
2023-05-22 08:40:08 +0700

    Major UI Fixes

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 const form = document.querySelector("#searchForm");
 form.addEventListener('submit', async function (e) {
     e.preventDefault();
+    $("#show-container").empty(); 
     const searchTerm = form.elements.query.value;
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
     makeImages(res.data)
 })
 
+const show_container = document.querySelector("#show-container");
 const makeImages = (shows) => {
     for (let result of shows) {
         console.log(result);
         if (result.show.image) {
             const img = document.createElement('IMG');
             img.src = result.show.image.medium;
-            document.body.append(img);
+            show_container.append(img);
+            img.classList.add('m-2');
         }
     }
 }
